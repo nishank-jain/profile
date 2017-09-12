4f26852e4fd812e24a44a7be2eb218f2cd847e48
Nishank Jain <nishank.jain@btech2010.iitgn.ac.in>
2017-09-13 01:43:31 +0530

    Log env variable.

--- routes/01academics.js
+++ routes/01academics.js
@@ -4,6 +4,7 @@ var router = express.Router();
 // Home page route
 router.get('/', function(req, res) {
 	console.log("device :", req.device.type);
+	console.log("env: ", process.env.NODE_ENV);
 	res.render('academics/index', {
 		title: "Academics - Nishank Jain",
 		description: "Educational background, academic achievements, internships, projects and other activities.",
