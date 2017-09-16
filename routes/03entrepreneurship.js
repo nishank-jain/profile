1323ca10cf35d18ab6de44cb3676d27615ed7ad1
Nishank Jain <nishank.jain@btech2010.iitgn.ac.in>
2017-09-16 16:45:08 +0530

    Entrepreneurship section start.

---
+++ routes/03entrepreneurship.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+// Home page route
+router.get('/', function(req, res) {
+	res.render('entrepreneurship/index', {
+		title: "Entrepreneurship - Nishank Jain",
+		description: "Starting up a company - a sports venture called Courtside, building technology from ground up.",
+		topbar: false
+	});
+});
+
+module.exports = function (app) {
+	app.use('/entrepreneurship', router);
+};
