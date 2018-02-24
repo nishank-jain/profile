cf41105c23a9e6869d60fa681a82cd273e777f60
Nishank Jain <nishankjain@users.noreply.github.com>
2018-02-24 13:23:15 +0530

    Updated index.js and app.js file to enable image uploads.

--- projects/t-shirt-design-editor/app.js
+++ projects/t-shirt-design-editor/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 require('sqlite3');
 
 var index = require('./routes/index');
-var imageUpload = require('./routes/upload');
+// var imageUpload = require('./routes/upload');
 
 var models = require('./models');
 models.sequelize.sync();
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', index);
-app.use('/api', imageUpload);
+// app.use('/api', imageUpload);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
